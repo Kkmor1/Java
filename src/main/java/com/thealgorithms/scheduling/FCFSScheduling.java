8ff3024991fe697a44a8df712a68afa0cf421679
KKm <1771573280@qq.com>
2026-06-05 09:32:07 +0000

    test

--- src/main/java/com/thealgorithms/scheduling/FCFSScheduling.java
+++ src/main/java/com/thealgorithms/scheduling/FCFSScheduling.java
@@ -1,6 +1,7 @@
 package com.thealgorithms.scheduling;
 
 import com.thealgorithms.devutils.entities.ProcessDetails;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -16,6 +17,7 @@ public class FCFSScheduling {
     }
 
     public void scheduleProcesses() {
+        processes.sort(Comparator.comparingInt(ProcessDetails::getArrivalTime));
         evaluateWaitingTime();
         evaluateTurnAroundTime();
     }
@@ -27,21 +29,25 @@ public class FCFSScheduling {
             return;
         }
 
-        int waitingTime = 0;
+        int currentTime = processes.get(0).getArrivalTime();
         int burstTime = processes.get(0).getBurstTime();
 
-        processes.get(0).setWaitingTime(waitingTime); // for the first process, waiting time will be 0.
+        processes.get(0).setWaitingTime(0);
+        currentTime += burstTime;
 
         for (int i = 1; i < processesNumber; i++) {
-            processes.get(i).setWaitingTime(waitingTime + burstTime);
-            waitingTime = processes.get(i).getWaitingTime();
-            burstTime = processes.get(i).getBurstTime();
+            ProcessDetails currentProcess = processes.get(i);
+            if (currentTime < currentProcess.getArrivalTime()) {
+                currentTime = currentProcess.getArrivalTime();
+            }
+            currentProcess.setWaitingTime(currentTime - currentProcess.getArrivalTime());
+            currentTime += currentProcess.getBurstTime();
         }
     }
 
     private void evaluateTurnAroundTime() {
         for (final var process : processes) {
-            process.setTurnAroundTimeTime(process.getBurstTime() + process.getWaitingTime());
+            process.setTurnAroundTime(process.getBurstTime() + process.getWaitingTime());
         }
     }
 }
--- src/test/java/com/thealgorithms/scheduling/FCFSSchedulingTest.java
+++ src/test/java/com/thealgorithms/scheduling/FCFSSchedulingTest.java
@@ -20,21 +20,132 @@ public class FCFSSchedulingTest {
 
         assertEquals("P1", processes.get(0).getProcessId());
         assertEquals(0, processes.get(0).getWaitingTime());
-        assertEquals(10, processes.get(0).getTurnAroundTimeTime());
+        assertEquals(10, processes.get(0).getTurnAroundTime());
 
         assertEquals("P2", processes.get(1).getProcessId());
         assertEquals(10, processes.get(1).getWaitingTime());
-        assertEquals(15, processes.get(1).getTurnAroundTimeTime());
+        assertEquals(15, processes.get(1).getTurnAroundTime());
 
         assertEquals("P3", processes.get(2).getProcessId());
         assertEquals(15, processes.get(2).getWaitingTime());
-        assertEquals(23, processes.get(2).getTurnAroundTimeTime());
+        assertEquals(23, processes.get(2).getTurnAroundTime());
+    }
+
+    @Test
+    public void testingProcessesWithDifferentArrivalTimes() {
+        List<ProcessDetails> processes = addProcessesWithDifferentArrivalTimes();
+        final FCFSScheduling fcfsScheduling = new FCFSScheduling(processes);
+
+        fcfsScheduling.scheduleProcesses();
+
+        assertEquals(4, processes.size());
+
+        assertEquals("P1", processes.get(0).getProcessId());
+        assertEquals(0, processes.get(0).getWaitingTime());
+        assertEquals(5, processes.get(0).getTurnAroundTime());
+
+        assertEquals("P2", processes.get(1).getProcessId());
+        assertEquals(4, processes.get(1).getWaitingTime());
+        assertEquals(7, processes.get(1).getTurnAroundTime());
+
+        assertEquals("P3", processes.get(2).getProcessId());
+        assertEquals(3, processes.get(2).getWaitingTime());
+        assertEquals(7, processes.get(2).getTurnAroundTime());
+
+        assertEquals("P4", processes.get(3).getProcessId());
+        assertEquals(2, processes.get(3).getWaitingTime());
+        assertEquals(8, processes.get(3).getTurnAroundTime());
+    }
+
+    @Test
+    public void testingProcessesAddedInRandomOrder() {
+        List<ProcessDetails> processes = addProcessesInRandomOrder();
+        final FCFSScheduling fcfsScheduling = new FCFSScheduling(processes);
+
+        fcfsScheduling.scheduleProcesses();
+
+        assertEquals(3, processes.size());
+
+        assertEquals("P3", processes.get(0).getProcessId());
+        assertEquals(0, processes.get(0).getWaitingTime());
+        assertEquals(3, processes.get(0).getTurnAroundTime());
+
+        assertEquals("P1", processes.get(1).getProcessId());
+        assertEquals(1, processes.get(1).getWaitingTime());
+        assertEquals(6, processes.get(1).getTurnAroundTime());
+
+        assertEquals("P2", processes.get(2).getProcessId());
+        assertEquals(4, processes.get(2).getWaitingTime());
+        assertEquals(9, processes.get(2).getTurnAroundTime());
+    }
+
+    @Test
+    public void testingProcessWithGapBetweenArrivals() {
+        List<ProcessDetails> processes = addProcessesWithGaps();
+        final FCFSScheduling fcfsScheduling = new FCFSScheduling(processes);
+
+        fcfsScheduling.scheduleProcesses();
+
+        assertEquals(3, processes.size());
+
+        assertEquals("P1", processes.get(0).getProcessId());
+        assertEquals(0, processes.get(0).getWaitingTime());
+        assertEquals(5, processes.get(0).getTurnAroundTime());
+
+        assertEquals("P2", processes.get(1).getProcessId());
+        assertEquals(5, processes.get(1).getWaitingTime());
+        assertEquals(8, processes.get(1).getTurnAroundTime());
+
+        assertEquals("P3", processes.get(2).getProcessId());
+        assertEquals(0, processes.get(2).getWaitingTime());
+        assertEquals(4, processes.get(2).getTurnAroundTime());
     }
 
     private List<ProcessDetails> addProcessesForFCFS() {
         final ProcessDetails process1 = new ProcessDetails("P1", 0, 10);
         final ProcessDetails process2 = new ProcessDetails("P2", 1, 5);
         final ProcessDetails process3 = new ProcessDetails("P3", 2, 8);
+
+        final List<ProcessDetails> processDetails = new ArrayList<>();
+        processDetails.add(process1);
+        processDetails.add(process2);
+        processDetails.add(process3);
+
+        return processDetails;
+    }
+
+    private List<ProcessDetails> addProcessesWithDifferentArrivalTimes() {
+        final ProcessDetails process1 = new ProcessDetails("P1", 0, 5);
+        final ProcessDetails process2 = new ProcessDetails("P2", 1, 3);
+        final ProcessDetails process3 = new ProcessDetails("P3", 2, 4);
+        final ProcessDetails process4 = new ProcessDetails("P4", 3, 6);
+
+        final List<ProcessDetails> processDetails = new ArrayList<>();
+        processDetails.add(process1);
+        processDetails.add(process2);
+        processDetails.add(process3);
+        processDetails.add(process4);
+
+        return processDetails;
+    }
+
+    private List<ProcessDetails> addProcessesInRandomOrder() {
+        final ProcessDetails process1 = new ProcessDetails("P1", 2, 5);
+        final ProcessDetails process2 = new ProcessDetails("P2", 5, 5);
+        final ProcessDetails process3 = new ProcessDetails("P3", 0, 3);
+
+        final List<ProcessDetails> processDetails = new ArrayList<>();
+        processDetails.add(process1);
+        processDetails.add(process2);
+        processDetails.add(process3);
+
+        return processDetails;
+    }
+
+    private List<ProcessDetails> addProcessesWithGaps() {
+        final ProcessDetails process1 = new ProcessDetails("P1", 0, 5);
+        final ProcessDetails process2 = new ProcessDetails("P2", 10, 3);
+        final ProcessDetails process3 = new ProcessDetails("P3", 15, 4);
 
         final List<ProcessDetails> processDetails = new ArrayList<>();
         processDetails.add(process1);
